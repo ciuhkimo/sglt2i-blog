cec196739c5ef55d3394b52b73848faea173cb42
張育興 <yu-hsing@zhangyuxingdeMacBook-Pro.local>
2026-04-04 21:11:54 +0800

    fix: 加入 robots.txt + 修正 site URL 讓 sitemap 正確

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://sglt2i-blog.vercel.app',
 	integrations: [mdx(), sitemap()],
 });
